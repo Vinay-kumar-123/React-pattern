be9669ded949697525b07967041a1ce152a288a1
vinay-kumar-123 <kingboy92785@gmail.com>
2025-03-26 02:47:22 +0530

    change header file

--- src/components/Matrix.jsx
+++ src/components/Matrix.jsx
@@ -1,36 +1,53 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Matrix = () => {
-    const [boxes, setBoxes] = useState(Array(9).fill("white"));
-    const [order, setOrder] = useState([]);
+  const [boxes, setBoxes] = useState(Array(9).fill("white"));
+  const [order, setOrder] = useState([]);
 
-    const handleClick = (index) => {
-        if(boxes[index] === "white"){
-            const newBoxes = [...boxes];
-            newBoxes[index] = "green";
-            setBoxes(newBoxes);
-            setOrder([...order, index])
-        }
+  const handleClick = (index) => {
+    if (boxes[index] === "white") {
+      const newBoxes = [...boxes];
+      newBoxes[index] = "green";
+      setBoxes(newBoxes);
+      setOrder([...order, index]);
     }
-    if(order.length === 9){
-        order.forEach((j,i) => {
-            setTimeout(() => {
-                setBoxes((pre) => {
-                    let updated = [...pre];
-                    updated[j] = 'orange';
-                    return updated;
-                });
-            }, (i + 1)*400);
-        })
-    }
+  };
+  if (order.length === 9) {
+    order.forEach((j, i) => {
+      setTimeout(() => {
+        setBoxes((pre) => {
+          let updated = [...pre];
+          updated[j] = "orange";
+          return updated;
+        });
+      }, (i + 1) * 400);
+    });
+  }
 
   return (
-    <div className='grid'>
-      {boxes.map((color, index) => (
-        <div className='box' key={index} style={{backgroundColor: color}} onClick={() => handleClick(index)}></div>
-      ))}
-    </div>
-  )
-}
+    <>
+      <div className="container">
+        <h1>Interactive Color Changing Grid in React</h1>
+        <p >
+          This project is a React-based interactive 3x3 grid, where each box
+          changes color when clicked. Initially, all boxes are white, and when
+          clicked, they turn green. The sequence of clicked boxes is stored, and
+          once all 9 boxes are clicked, they change to orange one by one in the
+          same order.
+        </p>
+      </div>
+      <div className="grid">
+        {boxes.map((color, index) => (
+          <div
+            className="box"
+            key={index}
+            style={{ backgroundColor: color }}
+            onClick={() => handleClick(index)}
+          ></div>
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default Matrix
+export default Matrix;
